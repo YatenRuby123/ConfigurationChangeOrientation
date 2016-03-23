d1405c9fa710f9454c3d3d3978770d24fc5621fe
Yatendra Pawar <rubyyatpaw2100@gmail.com>
2016-03-22 17:38:49 -0700

    Initial commit

---
+++ app/src/main/java/com/toast/example/supershuttle/configurationchangeorientation/MainActivity.java
@@ -0,0 +1,30 @@
+package com.toast.example.supershuttle.configurationchangeorientation;
+
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+
+        if (newConfig.orientation == Configuration.ORIENTATION_PORTRAIT) {
+
+            Toast.makeText(getApplicationContext(), "Portrait mode", Toast.LENGTH_SHORT).show();
+
+        } else if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+
+            Toast.makeText(getApplicationContext(), "Landscape mode", Toast.LENGTH_SHORT).show();
+        }
+    }
+}
